bbffffbdba20bed3bb996460e6dc65e80570151e
Kevin <moralesperezkevin7@gmail.com>
2024-11-09 08:53:14 -0600

    Creando las variables de entorno, así como la respectiva conexión

---
+++ config.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+dotenv.config();
+const dbURI = process.env.MONGODB_URI
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbURI);
+    console.log('Conectado a la base de datos MongoDB');
+  } catch (err) {
+    console.error('Error al conectarse:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
