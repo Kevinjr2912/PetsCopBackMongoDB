fd96d9fcdbba6f11bae96ddc993509a3deb4bb82
Kevin <moralesperezkevin7@gmail.com>
2024-11-09 08:56:17 -0600

    Creando el server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectDB = require('./config');
+const dotenv = require('dotenv');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
