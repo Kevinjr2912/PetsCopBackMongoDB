38d1cb6d3dc4b6ac4baa3ab241514822721dc3b1
Kevin <moralesperezkevin7@gmail.com>
2024-11-09 22:53:46 -0600

    Implementando la funcionalidad de obtener los servicios y locales, así como eliminar uno del sistema

--- src/routes/LocalService.routes.js
+++ src/routes/LocalService.routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { registerLocalService, getInformationLocalService } = require('../controllers/LocalService.controller');
+const { registerLocalService, getInformationLocalService, getInformationAllLocalsAndServices, deleteLocalOrService } = require('../controllers/LocalService.controller');
 
 // Route to create a local or service
 router.post('/registerLocalService', registerLocalService);
 
 // Route to get information fo a local or service
 router.post('/getInformationLocalService/:id_local_service', getInformationLocalService)
+
+// Route to get all locals and services
+router.get('/getAllLocalsServices', getInformationAllLocalsAndServices);
+
+// Route to delete a local o service of the system
+router.delete('/deleteLocalOrService/:id_local_service', deleteLocalOrService);
 
 module.exports = router;
