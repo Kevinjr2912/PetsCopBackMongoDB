07fe52b8cce0f829039dacc413629a4ef1240f38
Kevin <moralesperezkevin7@gmail.com>
2024-11-11 03:16:53 -0600

    Cambio inperceptible

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectDB = require('./config');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const postRoute = require('./src/routes/Post.route');
 const localService = require('./src/routes/LocalService.routes');
